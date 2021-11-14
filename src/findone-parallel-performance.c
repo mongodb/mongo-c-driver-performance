c44984d0d046de8f04e16f8e9d4ddaa213fa98a6
Kevin Albertson <kevin.albertson@mongodb.com>
2021-11-14 12:39:26 -0500

    spawn N threads in findone_parallel_perf_task

--- src/findone-parallel-performance.c
+++ src/findone-parallel-performance.c
@@ -20,20 +20,60 @@
 
 #include "mongo-c-performance.h"
 
+#include <bson.h>
 #include <mongoc.h>
+
+#include <pthread.h>
+
+typedef struct {
+   pthread_t thread;
+} findone_parallel_thread_context_t;
 
 typedef struct {
    perf_test_t base;
    mongoc_client_pool_t *pool;
+   int nthreads;
+   findone_parallel_thread_context_t *contexts;
 } findone_parallel_perf_test_t;
 
-static void findone_parallel_perf_task (perf_test_t *pt) {
+static void* _findone_parallel_perf_thread (void* p) {
+   findone_parallel_thread_context_t *ctx = (findone_parallel_thread_context_t*) p;
+
    int i;
    int computation = 1;
 
    for (i = 0; i < 100; i++) {
       computation = computation + 1 * 2;
       usleep (10);
+   }
+   return NULL;
+}
+
+static void findone_parallel_perf_task (perf_test_t *test) {
+   findone_parallel_perf_test_t *findone_parallel_test = (findone_parallel_perf_test_t*) test;
+   int i;
+   int ret;
+
+   for (i = 0; i < findone_parallel_test->nthreads; i++) {
+      findone_parallel_thread_context_t* ctx;
+
+      ctx = &findone_parallel_test->contexts[i];
+      ret = pthread_create (&ctx->thread, NULL /* attr */, _findone_parallel_perf_thread, &ctx);
+      if (ret != 0) {
+         MONGOC_ERROR ("Error: pthread_create returned %d", ret);
+         abort ();
+      }
+   }
+
+   for (i = 0; i < findone_parallel_test->nthreads; i++) {
+      findone_parallel_thread_context_t* ctx;
+
+      ctx = &findone_parallel_test->contexts[i];
+      ret = pthread_join (ctx->thread, NULL /* out */);
+      if (ret != 0) {
+         MONGOC_ERROR ("Error: pthread_join returned %d", ret);
+         abort ();
+      }
    }
 }
 
@@ -45,6 +85,7 @@ static void findone_parallel_perf_setup (perf_test_t *test) {
 
    uri = mongoc_uri_new (NULL);
    findone_parallel_test->pool = mongoc_client_pool_new (uri);
+   findone_parallel_test->contexts = (findone_parallel_thread_context_t*) bson_malloc0 (findone_parallel_test->nthreads * sizeof (findone_parallel_thread_context_t));
 
    mongoc_uri_destroy (uri);
 }
@@ -54,6 +95,7 @@ static void findone_parallel_perf_teardown (perf_test_t *test) {
 
    MONGOC_DEBUG ("findone_parallel_perf_teardown");
    mongoc_client_pool_destroy (findone_parallel_test->pool);
+   bson_free (findone_parallel_test->contexts);
 }
 
 static perf_test_t *
@@ -62,6 +104,8 @@ findone_parallel_perf_new (const char *name, int nthreads)
    findone_parallel_perf_test_t *findone_parallel_test =
       bson_malloc0 (sizeof (findone_parallel_perf_test_t));
    perf_test_t *test = (perf_test_t *) findone_parallel_test;
+
+   findone_parallel_test->nthreads = nthreads;
 
    perf_test_init (test,
                    name,
